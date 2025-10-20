1c6e0c42b79e28ab3fccb323356dd0cd0b4e85e2
Ieuan1005 <ieuan.bergelin@outlook.com>
2025-10-20 20:28:01 +0100

    made basic object darkening with shader graph TODO - add texture version and look at making fire spread on flammable floor

--- Assets/Scripts/Flammable.cs
+++ Assets/Scripts/Flammable.cs
@@ -11,35 +11,39 @@ public class Flammable : MonoBehaviour
 
     
 
-    [SerializeField] int health = 10;
+    [SerializeField] float health = 10f;
     //[SerializeField] int damage = 4;
     //[SerializeField] float insulation = 1f;
-    [SerializeField] int temperature = 1; 
-    [SerializeField] int ignitionPoint = 10;
+    [SerializeField] float temperature = 1f; 
+    [SerializeField] float ignitionPoint = 10f;
 
     //[SerializeField] bool isFlammable;
     [SerializeField] bool onFire; 
 
-    float ignitiontime  = 0f;
-    float ignitiontick = 0.26f;
-    float damagetick = 1f;
-    float damagetime = 0f;
-    float darkenValue = 0f;  
+    float temptime  = 0f; //for resetting timer, could be local variable
+    float temptick = 0.26f; //how often temp increases
+    float damagetick = 0.17f; //how often object takes fire damage 
+    float damagetime = 0f; //for resetting timer, could be local variable
+    float darkenvalue;
+    float starthealth;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform.GetChild(0).gameObject.SetActive(false);
-        gameObject.GetComponentInParent<Renderer>().sharedMaterial.SetFloat("_Darken", darkenValue); //PUT WORK IN GITHUB REPO
+        //gameObject.GetComponentInParent<Renderer>().sharedMaterial.SetFloat("_Darken", darkenValue); 
+        starthealth = health;
     }
 
     // Update is called once per frame
     void Update()
     {
+       DarkenObject();
        startFire();
        Debug.Log("temp =" + temperature);
-       Debug.Log("hp =" + health); 
+       Debug.Log("hp =" + health);
+       Debug.Log("darken =" + darkenvalue); 
     }
 
     private void OnTriggerStay(Collider other)    
@@ -48,9 +52,9 @@ public class Flammable : MonoBehaviour
         {
             Debug.Log("Fire detected");
             
-            if ((Time.time - ignitiontime) > ignitiontick)
+            if ((Time.time - temptime) > temptick)
             {
-                ignitiontime = Time.time;
+                temptime = Time.time; 
 
                 temperature++;
             }
@@ -67,7 +71,7 @@ public class Flammable : MonoBehaviour
         Burning();
     }
 
-    public void Burning()
+    void Burning()
     {
         if (onFire)
         {
@@ -84,21 +88,26 @@ public class Flammable : MonoBehaviour
 
          
     }
-    public void TakeDamage()
+    void TakeDamage()
     {
         if ((Time.time - damagetime) > damagetick)
         {
             damagetime = Time.time; 
 
-            health -= 3; 
+            health -= 0.5f; 
         }
 
-        if (health <= 0)
+        if (health <= 0f)
         {
             Destroy(transform.parent.gameObject); 
         }
     }
 
+    void DarkenObject()
+    {
+        darkenvalue = health/starthealth;
+        gameObject.GetComponentInParent<Renderer>().sharedMaterial.SetFloat("_Darken", darkenvalue);  
+    }
 
     /*public void burning()
     {
